04d4c88aa7e0bde34083152825057b90c1f3343d
sok <38783929+Sokmean09@users.noreply.github.com>
2024-08-25 15:59:35 +0700

    fix ui

--- CookbookApplication/Views/RecipeView.xaml.cs
+++ CookbookApplication/Views/RecipeView.xaml.cs
@@ -15,10 +15,5 @@ namespace CookbookApplication.Views
 
             DataContext = new RecipeViewModel(new DefaultDialogService(), new DocDocxFileService(), new PdfFileService(), new JsonFileService());
         }
-
-        private void ListBox_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
-        {
-
-        }
     }
 }
